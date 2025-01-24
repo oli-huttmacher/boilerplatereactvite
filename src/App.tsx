44cceb3186749e0e7a2211f2d3544b18539d38c1
olihuttmacher <olihuttmacher@gmail.com>
2025-01-24 21:07:38 +0100

    premiere partie react vite tailwind config et alias

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='m-4 p-4 bg-mint-500 font-mono'>
+        <h1>hello</h1>
+        <h2>c'est moi comment vas les gens</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
